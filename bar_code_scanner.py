060864edb7995c572ccde5311be7b8b63a75bf92
bajpaidevharsh <115648107+bajpaidevharsh@users.noreply.github.com>
2024-06-28 23:10:33 +0530

    bar_code_scanner.py

--- bar_code_scanner.py
+++ bar_code_scanner.py
@@ -4,11 +4,11 @@ from pyzbar import pyzbar
 
 
 
-image1=cv2.imread("C:/Users/DEV HARSH BAJPAI/Downloads/bar-code.webp")
+image1=cv2.imread("#location")
 
 image1=cv2.resize(image1,(500,500))
 
-image2=cv2.imread("C:/Users/DEV HARSH BAJPAI/Downloads/bar3.jpg")
+image2=cv2.imread("location")
 image2=cv2.resize(image2,(500,500))
 image3=numpy.hstack((image1,image2))
 code=pyzbar.decode(image3)
